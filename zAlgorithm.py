89a22bd46d44054b813379030b3686b879c8386c
epennell <e-penn@hotmail.com>
2017-09-09 12:55:55 -0600

    Add files via upload

--- zAlgorithm.py
+++ zAlgorithm.py
@@ -6,27 +6,31 @@ endprefix = "5"
 LP = len(pattern)
 TP = len(text)
 
-# concatenate
+# Concatenate
 pattext = pattern + endprefix + text
-print(pattext)
 LPT = len(pattext)
 
-Zlist = [0,0]
+Zlist = [0]
+
+exactMatches = 0
+zListIndex = []
 
 l = 0
 r = 0
-for k in range(1,LPT):
+for k in range(1,LPT-1):
     if k>r: # case 1
         i = 0
         ksub = k
-        print(Zlist)
-        while pattext[k] == pattext[i]:
+        while ksub < LPT and pattext[ksub] == pattext[i]:
             i=i+1
-#            ksub=ksub+1
+            ksub=ksub+1
         Zlist.insert(k,i)
+        if LP == i:    # There is an exact match
+            exactMatches += 1  # The number of exact matches
+            zListIndex.append(k)    # the indeces of those exact matches
         l=k
         r=k+Zlist[k]
-    else: # else if k <= r: case 2
+    else: # else if k <= r -- case 2
         beta = pattext[k:r]
         # position of matching box
         kp = k-l+1  #kprime or k'
@@ -41,3 +45,5 @@ for k in range(1,LPT):
             Zlist.insert(k,i-k)
             r=i-1
             l=k
+
+print("The number of exact matches was ", exactMatches, " and the index(es) of the exact matches is/are ", zListIndex)
