f09477c38ebbc79d19abceee6ad6508501d2f4ed
epennell <e-penn@hotmail.com>
2017-09-24 17:38:09 -0600

    Add files via upload

--- Smith_Waterman.py
+++ Smith_Waterman.py
@@ -2,6 +2,12 @@
 #   CSCI 451, HW 2 Problem 1
 #   Emerald Ellis, Elizabeth Pennell, and Jake Petek
 
+s = "tgt"
+t = "ctgtact"
+slen = len(s)+1
+tlen=len(t)+1
+v = [[0 for item in range(tlen)] for i in range(slen)]
+
 def match(s,t):
     if s is t:
         return 2
@@ -11,24 +17,29 @@ def match(s,t):
 def smith_waterman(s, t):
     i = len(s)
     j = len(t)
-    v = [[0 for x in range(i)] for y in range(j)]
     # a of s and b of t have highest global alignment score
     # V(i,j) is max score of the global alignment of A and B over S and T
     # Base case:
     if i is 0:
-        v[0][j] = 0
+        v[i][j] = 0
+        if j is 0:
+            v[i][j] = 0
         return v
     elif j is 0:
-        v[i][0] = 0
+        v[i][j] = 0
         return v
     else:
         one = 0
-        two = smith_waterman(s[:i-1],t[:j-1])[i-1][j-1]+match(s[i],t[j])
-        three = smith_waterman(s[:i-1],t[:j])[i-1][j]-1
-        four = smith_waterman(s[:i],t[:j-1])[i][j-1]-1
+        v2 =smith_waterman(s[:i-1],t[:j-1])
+        mat = match(s[i-1],t[j-1])
+        two = v2[i-1][j-1]+mat
+        v3 = smith_waterman(s[:i-1],t[:j])
+        three = v3[i-1][j]-1
+        v4 = smith_waterman(s[:i],t[:j-1])
+        four = v4[i][j-1]-1
         v[i][j] = max(one, two, three, four)
         return v
         
-s = "act"
-t = "ctg"
 v = smith_waterman(s,t)
+print(v)
+print(max(max(v)))
