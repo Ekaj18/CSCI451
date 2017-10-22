b7e48cdf226eed2989035b4bf6f5074cdd7771ee
epennell <e-penn@hotmail.com>
2017-10-22 16:08:07 -0600

    Add files via upload
    Jake's solution!

--- FM_Index.py
+++ FM_Index.py
@@ -1,5 +1,6 @@
 #FM-Index Implementation
-
+#S="acgtgccta$"
+D="$acgt"
 def fm_index(Q,BW,C,occ):
     x = Q[end]
     i = len(Q)-1
@@ -14,4 +15,28 @@ def fm_index(Q,BW,C,occ):
         print("No valid comparisons")
     else:
         print("["+str(st)+", "+str(ed)+"] is the parameter for a match")
-        
+
+def getSuffix(index):
+    return S[index:]
+
+def getBW(index):
+    if index == 0:
+        return S[len(S)-1]
+    return S[index-1]
+
+def build_BW():
+    SA = list(range(len(S)))
+    SA = sorted(SA, key=getSuffix)
+    BW = [getBW(index) for index in SA]
+    C = {}
+    for i in range(len(D)):
+        if i!= 0:
+            C[D[i]] = C[D[i-1]] + S.count(D[i-1])
+        else:
+            C[D[i]] = 0
+    print(C)
+
+file = open("text.txt","r")
+S = file.read() + "$"
+S = S.lower()
+build_BW()
